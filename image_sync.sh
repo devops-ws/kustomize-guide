7e4fdf4d3d4c6b9b4740fb527bf363136ee2fc0b
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2023-04-17 12:42:23 +0800

    Update image_sync.sh

--- image_sync.sh
+++ image_sync.sh
@@ -17,6 +17,12 @@ function syncToLocalServer() {
             docker pull $source
             docker tag $source $line
             docker push $line
+        elif [[ "$line" =~ ^10.121.218.184:30002/docker.elastic.co.* ]]; then
+            source=$(echo $line | sed -e "s/10.121.218.184:30002\///")
+            echo "start to sync from $source to $line"
+            docker pull $source
+            docker tag $source $line
+            docker push $line
         fi
     done < bin/image.txt
 }
@@ -24,13 +30,16 @@ function syncToLocalServer() {
 
 function saveToTar() {
     kustomize build config/external | grep image: | sed -e "s/-\ image: //" | sed -e "s/image: //" | sed -e "s/\s*//" | sort | uniq > bin/image.txt
+    rm -rf bin/ids.txt
 
     while read -r line
     do
         echo "start to save $line"
         docker pull $line
-        docker image save $line -o bin/$(echo $line | sed -e "s/.*\///" | sed -e "s/:.*//").tar.gz
+        docker inspect $line | jq .[0].Id | sed -e "s/\"//g" | sed -e "s/null//" >> bin/ids.txt
+        # docker image save $line -o bin/$(echo $line | sed -e "s/.*\///" | sed -e "s/:.*//").tar.gz
     done < bin/image.txt
+    docker image save $(cat bin/ids.txt) -o bin/images.tar.gz
 }
 
 if [ "$1" == 'sync' ]; then
@@ -41,4 +50,3 @@ else
   echo "Usage: image_sync.sh [save|sync]"
   exit 1
 fi
-
